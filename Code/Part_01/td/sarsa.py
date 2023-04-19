fb6845a40bdb4e515e21a3dda0bdc25e10f09742
Ahmed-aq <a.s.qayyum11@gmail.com>
2023-04-19 18:36:05 +0100

    Implemented sarsa for 2g

--- Code/Part_01/td/sarsa.py
+++ Code/Part_01/td/sarsa.py
@@ -38,4 +38,25 @@ class SARSA(TDController):
         # Q value estimate in the base class and will update
         # the greedy policy and estimated state value function
         
-        pass
+        for i in range(episode._number_of_steps - 1):
+
+            # get the state, action and reward for the current step of the episode
+            current_state = episode.state(i)
+            current_action = episode.action(i)
+            current_reward = episode.reward(i)
+            next_state = episode.state(i+1)
+            next_action = episode.state(i+1)
+
+            # get the coords of the current state and the old value of Q at the current state
+            xy = current_state.coords()
+            old_Q = self._Q[xy[0], xy[1], current_action]
+
+            # get the coords of the next state and the value of Q at the next state 
+            next_xy = next_state.coords()
+            next_Q = self._Q[next_xy[0], next_xy[1], next_action]
+
+            # calculate the new value of Q 
+            error = current_reward + self.gamma() * next_Q - old_Q
+            new_Q = old_Q + self.alpha() * error
+
+            self._update_q_and_policy(xy, current_action, new_Q) 
