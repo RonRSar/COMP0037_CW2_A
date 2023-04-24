b03265ce17e41b0146971fa911540d687582d8dd
RonitS <zcemrrs@ucl.ac.uk>
2023-04-24 12:28:20 +0100

    q2c q2e code

--- Code/Part_01/q2_c.py
+++ Code/Part_01/q2_c.py
@@ -40,14 +40,14 @@ if __name__ == '__main__':
 
     # These values worked okay for me.
     policy_learner.set_alpha(0.1)
-    policy_learner.set_experience_replay_buffer_size(64)
+    policy_learner.set_experience_replay_buffer_size(32)
     policy_learner.set_number_of_episodes(32)
     
     # The drawers for the state value and the policy
     value_function_drawer = ValueFunctionDrawer(policy_learner.value_function(), drawer_height)    
     greedy_optimal_policy_drawer = LowLevelPolicyDrawer(policy_learner.policy(), drawer_height)
     
-    for i in range(40):
+    for i in range(1000):
         print(i)
         policy_learner.find_policy()
         value_function_drawer.update()
@@ -55,6 +55,7 @@ if __name__ == '__main__':
         pi.set_epsilon(1/math.sqrt(1+0.25*i))
         print(f"epsilon={1/math.sqrt(1+i)};alpha={policy_learner.alpha()}")
 
+    airport_map_drawer.save_screenshot(f"corridor.pdf")
     value_function_drawer.save_screenshot(f"2c_value.pdf")
     greedy_optimal_policy_drawer.save_screenshot(f"2c_greedy.pdf")
 
